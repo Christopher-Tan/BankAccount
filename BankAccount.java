86707922ea82ddfb0d6d74bb5b793fa3fb5cd53d
Christopher Tan <ctan20@stuy.edu>
2020-10-27 19:27:46 -0400

    Created and tested getBalance and getAccountID methods

--- BankAccount.java
+++ BankAccount.java
@@ -7,4 +7,10 @@ public class BankAccount {
         password = pass;
         balance = 0;
     }
+    public double getBalance() {
+        return balance;
+    }
+    public int getAccountID() {
+        return accountID;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,7 @@
 public class Tester {
     public static void main(String[] args) {
         BankAccount test = new BankAccount(10,"Password");
+        System.out.println(test.getBalance());
+        System.out.println(test.getAccountID());
     }
 }
