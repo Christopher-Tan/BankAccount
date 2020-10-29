42c27d6671835256ae8520b792467a8011ddb475
Christopher Tan <ctan20@stuy.edu>
2020-10-28 23:20:06 -0400

    Added transferTo method from class

--- BankAccount.java
+++ BankAccount.java
@@ -33,6 +33,12 @@ public class BankAccount {
         }
     }
     public String toString() {
-        return accountID + "\t" + balance;
+        return "#" + accountID + "\t" + "$" + balance;
+    }
+    private boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+    public boolean transferTo(BankAccount other, double amount, String password) {
+        return (authenticate(password) && withdraw(amount) && other.deposit(amount));
     }
 }
